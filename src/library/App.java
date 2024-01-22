94648a317cd3aca74d015f751a91b949cbf51b83
andyking <andrewk640@gmail.com>
2024-01-22 10:45:26 +0000

    created add and remove for Library and remove by ID

--- src/library/App.java
+++ src/library/App.java
@@ -1,17 +1,29 @@
 package library;
 
-import java.util.ArrayList;
-
 public class App {
 	public static void main(String[] args) {
 
-		ArrayList<Item> items = new ArrayList<>();
+//		ArrayList<Item> items = new ArrayList<>();
+//
+//		items.add(new Book("Harry Potter", "26-06-1997", 223, false));
+//		items.add(new Newspaper("Metro", "22-01-2024", 40, true));
+//		items.add(new Magazine("Kerrang", "11-03-2012", 50, true));
+//
+//		System.out.println(items);
 
-		items.add(new Book("Harry Potter", "26-06-1997", 223, false));
-		items.add(new Newspaper("Metro", "22-01-2024", 40, true));
-		items.add(new Magazine("Kerrang", "11-03-2012", 50, true));
+		Library myLibrary = new Library();
 
-		System.out.println(items);
+		myLibrary.addToLibrary(new Book("Harry Potter", "26-06-1997", 223, false));
+		myLibrary.addToLibrary(new Newspaper("Metro", "22-01-2024", 40, true));
+		myLibrary.addToLibrary(new Magazine("Kerrang", "11-03-2012", 50, true));
+
+		System.out.println("myLibrary: " + myLibrary);
+
+		myLibrary.removeFromLibrary(new Magazine("Kerrang", "11-03-2012", 50, true));
+
+		myLibrary.removeById(3);
+
+		System.out.println("myLibrary: " + myLibrary);
 
 	}
 }
--- src/library/Library.java
+++ src/library/Library.java
@@ -1,5 +1,33 @@
 package library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+
+	private ArrayList<Item> items = new ArrayList<>();
+
+	public void addToLibrary(Item item) {
+		items.add(item);
+	}
+
+	public void removeFromLibrary(Item item) {
+		items.remove(item);
+	}
+
+	public boolean removeById(int id) {
+		List<Item> toRemove = new ArrayList<Item>();
+		for (Item item : this.items) {
+			if (item.getId() == id) {
+				toRemove.add(item);
+			}
+		}
+		return this.items.removeAll(toRemove);
+	}
+
+	@Override
+	public String toString() {
+		return "items=" + items + "]";
+	}
 
 }
